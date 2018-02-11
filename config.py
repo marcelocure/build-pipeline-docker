a7ec38d14238f841a990c443e6ee4f56e7866936
Marcelo Cure <marcelo.cure@ilegra.com>
2018-02-11 18:46:44 -0200

    Making config dynamic

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
-registry = "localhost:5000"
-user = "curecure"
-base_dir = "/home/marcelocure/github/"
+import os
+
+def get_environment_variable(name):
+	try:
+		return os.environ[name]
+	except:
+		return None
+
+registry = get_environment_variable("REGISTRY") or "localhost:5000"
+user = get_environment_variable("USER") or "curecure"
+base_dir = get_environment_variable("BASE_DIR") or "/home/marcelocure/github/"
